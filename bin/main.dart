2794e768ed9368ed3a6cefe39e020d6b934010f7
Wilson, Marvin (jw9615) <jw9615@att.com>
2019-12-12 15:48:29 -0500

    parameterize inputs and outputs

--- bin/main.dart
+++ bin/main.dart
@@ -6,23 +6,47 @@ import 'package:image/image.dart' as img;
 import 'package:quiver/core.dart';
 
 var hexes = <Hex>{};
+var TRANSPARENT = img.getColor(0, 0, 0, 0);
 
 main(List<String> arguments) {
   print('Hello world!');
 
-  var image = img.Image(4800, 6400);
-  image.fill(img.getColor(127, 127, 255, 0));
+  // input parameters
+  // var pixelsPerYard = 120.0;
+  var pixelsPerYard = 120.0;
+  var toEdge = false;
+  var fname = 'The Strong Gate HoJ p50-hexes.png';
+  var color = img.getColor(255, 255, 0);
+  var thickness = 5;
 
-  var size = Point(69.2820323027551, 69.2820323027551);
-  var origin = Point(10, 10);
-  var layout = HexLayout.orientFlat(size, origin);
+  // give user the choice: print the hex image alone, or composite on top of other image.
+  // if hex image, require user enters dimensions (width x height). if composite, use
+  // source image dimensions.
+  // var height = 640;
+  // var width = 480;
+
+  var image = img.decodeImage(File(
+          '/Users/jw9615/Personal/docs/HoJ_20Encounter_20Maps/The Strong Gate HoJ p50.jpg')
+      .readAsBytesSync());
+
+  // Given a right triangle, ABC, where AB and AC are the sides and BC is the
+  // hypotenuse, find the hypotenuse of a right triangle where AB =
+  // pixelsPerYard/2.0, and angle ACB is 60 degrees.
+  var edge = (0.5 * pixelsPerYard) / sin(60.0 * pi / 180.0);
+
+  // var image = img.Image(width, height);
+  // image.fill(img.getColor(127, 127, 255));
+
+  var size = Point(edge, edge);
+  var origin = Point(10, 10); // inset from the edge a little bit
+  var layout = HexLayout.orientPointy(size, origin);
 
   for (var x = 0; x < image.width; x += size.x.toInt()) {
     for (var y = 0; y < image.height; y += size.y.toInt()) {
       var pt = Point(x, y);
       var hex = Hex.fromPoint(layout, pt);
 
-      if (allPointsAreIn(hex.corners(layout), image)) hexes.add(hex);
+      if (toEdge || allPointsAreIn(hex.corners(layout), image)) hexes.add(hex);
     }
   }
 
@@ -35,11 +59,11 @@ main(List<String> arguments) {
       var current = points[i];
       var next = points.last == current ? points[0] : points[i + 1];
 
-      _drawLine(image, current, next);
+      _drawLine(image, current, next, color, thickness);
     }
   }
 
-  File('temp.png').writeAsBytesSync(img.encodePng(image));
+  File(fname).writeAsBytesSync(img.encodePng(image));
   print('Exit?');
 }
 
@@ -66,11 +90,12 @@ class Line {
 
 var lines = <Line>{};
 
-void _drawLine(img.Image image, Point<num> current, Point<num> next) {
+void _drawLine(img.Image image, Point<num> current, Point<num> next, int color,
+    num thickness) {
   if (!lines.contains(Line(current, next))) {
     img.drawLine(image, current.x.round(), current.y.round(), next.x.round(),
-        next.y.round(), img.getColor(0, 0, 0),
-        thickness: 2.5, antialias: false);
+        next.y.round(), color,
+        thickness: thickness, antialias: false);
     lines.add(Line(current, next));
   }
 }
